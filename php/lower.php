8900157add65f8a49a4c3d74a47ef0fe4082e2f0
MichaelRedant <michael.redant2@telenet.be>
2021-03-12 23:17:16 +0100

    Updates

--- php/lower.php
+++
@@ -1,6 +0,0 @@
-<footer id="main-footer">
-        <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Xinu B.V. All Rights Reserved</p>
-    </footer>
-    <script src="js/main.js"></script>
-</body>
-</html>
